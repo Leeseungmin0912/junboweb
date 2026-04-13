4e9d8c398d9177d3edfbe227d70e73a527123137
Leeseungmin0912 <leesn0913@naver.com>
2026-04-13 15:34:58 +0900

    Final fix for admin design and whitenoise

--- fraud_prevent/fraud_prevent/settings.py
+++ fraud_prevent/fraud_prevent/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+# Render가 디자인 파일을 모을 장소를 지정해줍니다.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
