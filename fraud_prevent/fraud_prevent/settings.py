5310aaed529b118b1637db310ee8f9cd2c47963b
nanfu <155872533+Leeseungmin0912@users.noreply.github.com>
2026-04-13 16:03:35 +0900

    Update settings.py

--- fraud_prevent/fraud_prevent/settings.py
+++ fraud_prevent/fraud_prevent/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://junboweb.onrender.com',
+]
+
 
 # Application definition
 
@@ -118,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+# Default primary keyALLOWED_HOSTS field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
