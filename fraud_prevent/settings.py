d74d182a3627f335435641a7e16f11b2f9aeab47
Leeseungmin0912 <leesn0913@naver.com>
2026-04-27 15:42:08 +0900

    Fix: Update settings.py for ALLOWED_HOSTS and DEBUG

--- fraud_prevent/settings.py
+++ fraud_prevent/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-px+pca#62j1gwlo1b8f1b!ipu3#n$-$09e!iq6r=g!b8+)$#9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
